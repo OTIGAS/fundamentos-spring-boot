9ef661bc8406828e1e3503c28b25d2f783f578c2
OTIGAS <tfariagouvea@gmail.com>
2025-02-19 16:21:34 -0300

    first commit

---
+++ src/main/java/br/com/tiagogouvea/fundamentos_springboot/FundamentosSpringbootApplication.java
@@ -0,0 +1,11 @@
+package br.com.tiagogouvea.fundamentos_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosSpringbootApplication.class, args);
+	}
---
+++ src/test/java/br/com/tiagogouvea/fundamentos_springboot/FundamentosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagogouvea.fundamentos_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
