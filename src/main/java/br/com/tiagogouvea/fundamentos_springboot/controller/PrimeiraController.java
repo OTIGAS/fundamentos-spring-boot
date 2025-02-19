2a302ff63afc69c91be3ca0601ee15d39427c386
OTIGAS <tfariagouvea@gmail.com>
2025-02-19 16:39:53 -0300

    Component Scan

--- src/main/java/br/com/tiagogouvea/fundamentos_springboot/controller/PrimeiraController.java
+++ src/main/java/br/com/tiagogouvea/fundamentos_springboot/controller/PrimeiraController.java
@@ -1,4 +1,4 @@
-package br.com.tiagogouvea.fundamentos_springboot;
+package br.com.tiagogouvea.fundamentos_springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
